0a4d940d9c7548702197eb5770482ef976467a16
Jordy Tshibangu <jordytshibangu@Admins-MacBook-Pro.local>
2022-01-25 18:12:04 +0200

    initial commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+  <>
+    <Nav>
+        <NavContainer>
+            <NavLogo></NavLogo>
+        </NavContainer>
+    </Nav>
+  </>
+  );
+};
+
+export default Navbar;
