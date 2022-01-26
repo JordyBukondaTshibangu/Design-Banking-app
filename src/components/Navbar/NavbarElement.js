a6e2627928fbac74b175960769c37d7d71eb6cb5
Jordy Tshibangu <jordytshibangu@Admins-MacBook-Pro.local>
2022-01-26 15:16:40 +0200

    features : navbar styling

---
+++ src/components/Navbar/NavbarElement.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+
+export const Nav = styled.nav``;
+export const NavContainer = styled.nav``;
+export const NavLogo = styled.nav``;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavContainer, Nav, NavLogo } from './NavbarElement';
 
 const Navbar = () => {
   return (
